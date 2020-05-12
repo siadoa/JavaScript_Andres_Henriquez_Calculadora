e7495868ecc14c8c9c587b6eedc5598638d200be
Andres Henriquez <siadoafh@gmail.com>
2020-05-12 01:36:34 -0500

    Agregar punto, insertar op basicas y cambiar signo

--- js/app.js
+++ js/app.js
@@ -18,6 +18,12 @@ let Calculadora = {
     document.getElementById('0').onclick = this.addCeros
     document.getElementById('on').onclick = this.limpiarPantalla
     document.getElementById('punto').onclick = this.agregarPunto
+    // document.getElementById('sign').onclick = this.cambiarSigno
+    document.getElementById('mas').onclick = this.operaciones
+    document.getElementById('menos').onclick = this.operaciones
+    document.getElementById('por').onclick = this.operaciones
+    document.getElementById('dividido').onclick = this.operaciones
+    document.getElementById('igual').onclick = this.igual
   },
   presionarBoton: (event) => {
     if (event.target.classList[0] == 'tecla') {
@@ -47,6 +53,63 @@ let Calculadora = {
     if (!display.innerHTML.includes('.')) {
       display.innerHTML = log + '.'
       log = log + '.'
+    }
+  },
+  operaciones: (event) => {
+    if (display.innerHTML !== '0') {
+      switch (event.target.id) {
+        case 'dividido':
+          display.innerHTML = ''
+          log = log + '/'
+          break
+
+        case 'por':
+          display.innerHTML = ''
+          log = log + '*'
+          break
+
+        case 'menos':
+          display.innerHTML = ''
+          log = log + '-'
+          break
+
+        case 'mas':
+          display.innerHTML = ''
+          log = log + '+'
+          break
+
+        default:
+          break
+      }
+    }
+  },
+  igual: (event) => {
+    if (display.innerHTML !== '0') {
+      let result
+      if (log.includes('+')) {
+        result = log
+          .split('+')
+          .map((el) => Number(el))
+          .reduce((a, b) => a + b)
+        display.innerHTML = result
+      }
+      if (log.includes('-')) {
+        result = log
+          .split('-')
+          .map((el) => Number(el))
+          .reduce((a, b) => a - b)
+        display.innerHTML = result
+      }
+    }
+  },
+  cambiarSigno: () => {
+    if (display.innerHTML !== '0') {
+      if (display.innerHTML.startsWith('-')) {
+        display.innerHTML = display.innerHTML.slice(1)
+        log = display.innerHTML
+      }
+      display.innerHTML = `-${display.innerHTML}`
+      log = display.innerHTML
     }
   },
 }
